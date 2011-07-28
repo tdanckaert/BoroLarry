b6823ab13481b82bef078e8ca6fc95f3b259496a
Thomas Danckaert <thomas.danckaert@gmail.com>
2011-07-29 00:58:35 +0200

    Can now push other robots

--- main.hs
+++ main.hs
@@ -36,33 +36,53 @@ data Robot = Robot { rcoords :: (Int,Int)
 -- Game state consists of list of robots and their positions.
 data Game = Game { bots :: Var [Robot] }
 
-robonames = ["spunky","bimbot"]
-startpos = [(4,5), (7,8)]
+robonames = ["spunky","bimbot","bimbot"]
+startpos = [(4,5), (7,8), (6,8)]
 
-moveBot vrobo n = do robo <- varGet vrobo
-                     varSet vrobo ( move n robo)
-                       where move n (Robot (x,y) d)
-                               | d == 0 = (Robot (x,y+n) d) 
-                               | d == 1 = (Robot (x+n,y) d)
-                               | d == 2 = (Robot (x,y-n) d)
-                               | d == 3 = (Robot (x-n,y) d)
+moveBot vrobos i n = do robos <- varGet vrobos
+                        varSet vrobos (tryMove robos i n)
+                        
+{- 
+tryMove: take the current list of Robots, and an order to move one of them,
+check if the target square is free, if not, try to move the robot in the target square as well, and then move
+-}
+tryMove robos i n = let (Robot pos d) = robos !! i
+                        move (x,y) -- translate robot movement order into a movement in a particular direction on the board
+                          | d == 0 = (x,y+n)
+                          | d == 1 = (x+n,y)
+                          | d == 2 = (x,y-n)
+                          | d == 3 = (x-n,y)
+                    in tryMove' robos i move
+
+tryMove' robos i move = let (Robot pos d) = robos !! i
+                            target = move pos
+                            robos' = (take i robos)
+                                     ++ (Robot target d) : drop (1+i) robos
+                        in case (roboAt target robos) of
+                          Nothing -> robos'
+                          Just j -> tryMove' (tryMove' robos j move) i move
+                                    
+roboAt target robos = findIndex ( \(Robot pos d) -> pos == target) robos
                                           
-rotateBot vrobo n = do (Robot pos dir) <- varGet vrobo
-                       varSet vrobo (Robot pos (rotate n dir))
+rotateBot vrobos i n = do robos <- varGet vrobos
+                          let (Robot pos direction) = robos !! i
+                              robo' = (Robot pos (rotate n direction))
+                              robos' = (take i robos) ++ robo' : (drop (1+i) robos)
+                            in varSet vrobos robos'
 
 makeCoord (x,y) = pt (x*40) (400 -(y*40))
 
 game = do f <- frameFixed [text := "Boro Larry"]
 --          robo <- varCreate (Robot (4,5) 0)
-          robos <- mapM (\p -> varCreate (Robot p 0)) startpos
+          vrobos <- varCreate $ map (\p -> (Robot p 0)) startpos
           sprites <- loadBitmaps
           tile <- bitmapCreateFromFile( "./tile.png")
-          p <- panel f [on paint := drawGame robos sprites tile]
+          p <- panel f [on paint := drawGame vrobos sprites tile]
           set f [ layout := minsize (sz maxX maxY) $ widget p]
-          set p [ on (charKey 'f') := (moveBot (robos !! 0) 1) >> repaint p
-                , on (charKey 'b') := (moveBot (robos !! 0) (-1)) >> repaint p
-                , on (charKey 't') := (rotateBot (robos !! 0) 1) >> repaint p
-                , on (charKey 'r') := (rotateBot (robos !! 0) (-1)) >> repaint p]
+          set p [ on (charKey 'f') := (moveBot vrobos 0 1) >> repaint p
+                , on (charKey 'b') := (moveBot vrobos 0 (-1)) >> repaint p
+                , on (charKey 't') := (rotateBot vrobos 0 1) >> repaint p
+                , on (charKey 'r') := (rotateBot vrobos 0 (-1)) >> repaint p]
 
 -- From the names in 'robonames' generate a list of lists with their sprites:
 -- [ [spunky0.png,spunky1.png,...,spunky3.png], [bimbot0.png, ....]]
@@ -70,11 +90,11 @@ loadBitmaps
   = sequence ( do roboname <- robonames
                   return ( mapM (\x -> bitmapCreateFromFile ( "./img/" ++ roboname ++ (show x) ++ ".png")) [0..3]))
     
-drawBot' dc vrobo sprites = do robo <- varGet vrobo
-                               let screencoords = (makeCoord  (rcoords robo))
-                                 in drawBitmap dc (sprites !! (direction robo)) screencoords True []
+drawBot' dc robo sprites = let screencoords = (makeCoord  (rcoords robo))
+                           in drawBitmap dc (sprites !! (direction robo)) screencoords True []
 
 drawGame vrobos sprites tile dc viewArea
   = do set dc [brushColor := black, brushKind := BrushSolid]
        mapM_ (\p -> drawBitmap dc tile p False []) coords
-       zipWithM_ (drawBot' dc) vrobos sprites 
+       robos <- varGet vrobos
+       zipWithM_ (drawBot' dc) robos sprites 
